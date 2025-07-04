031a510dbe94c5cb59fbc15da8a3ec65852a7d69
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-07-04 12:38:42 +0700

    feat(core): connect to MongoDB Atlas, setup config module and env support

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import databaseConfig from '@config/database.config';
+/* import { AppController } from './app.controller';
+import { AppService } from './app.service'; */
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/fallback',
+      }),
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Global ValidationPipe (xài class-validator)
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Trhgatu API')
+    .setDescription('NestJS backend template')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT);
+  console.log(`🚀 Server is running at http://localhost:${PORT}`);
 }
 bootstrap();
