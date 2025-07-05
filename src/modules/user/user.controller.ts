f0f40ad1f7564f765fdd1545b76e62168d1b02f4
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-07-05 23:08:49 +0700

    feat(user): add full RBAC guard and permissions for UserController

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -7,34 +7,45 @@ import {
   Patch,
   Delete,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto, UpdateUserDto, QueryUserDto } from './dtos';
+import { JwtAuthGuard } from '@modules/auth/guards';
+import { PermissionsGuard } from '@shared/guards/permissions.guard';
+import { Permissions } from '@shared/decorators';
+import { PermissionEnum } from '@shared/enums';
 
+@UseGuards(JwtAuthGuard, PermissionsGuard)
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Permissions(PermissionEnum.CREATE_USER)
   @Post('/create')
   create(@Body() dto: CreateUserDto) {
     return this.userService.create(dto);
   }
 
+  @Permissions(PermissionEnum.READ_USER)
   @Get('/')
   findAll(@Query() query: QueryUserDto) {
     return this.userService.findAll(query);
   }
 
+  @Permissions(PermissionEnum.READ_USER)
   @Get(':id')
   findById(@Param('id') id: string) {
     return this.userService.findById(id);
   }
 
+  @Permissions(PermissionEnum.UPDATE_USER)
   @Patch('/update/:id')
   update(@Param('id') id: string, @Body() dto: UpdateUserDto) {
     return this.userService.update(id, dto);
   }
 
+  @Permissions(PermissionEnum.DELETE_USER)
   @Delete('/delete/:id')
   remove(@Param('id') id: string) {
     return this.userService.remove(id);
