ff452e974f1e8d9adfda0248edc58d9437162907
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-07-05 22:54:41 +0700

    feat(role): add full RBAC guard and permissions for RoleController

--- src/modules/role/role.controller.ts
+++ src/modules/role/role.controller.ts
@@ -16,32 +16,36 @@ import { PermissionsGuard } from '@shared/guards/permissions.guard';
 import { CreateRoleDto, UpdateRoleDto, QueryRoleDto } from './dtos';
 import { JwtAuthGuard } from '@modules/auth/guards';
 
+@UseGuards(JwtAuthGuard, PermissionsGuard)
 @Controller('roles')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
-  @UseGuards(JwtAuthGuard, PermissionsGuard)
   @Permissions(PermissionEnum.CREATE_ROLE)
   @Post('/create')
   create(@Body() dto: CreateRoleDto) {
     return this.roleService.create(dto);
   }
 
+  @Permissions(PermissionEnum.READ_ROLE)
   @Get('/')
   findAll(@Query() query: QueryRoleDto) {
     return this.roleService.findAll(query);
   }
 
+  @Permissions(PermissionEnum.READ_ROLE)
   @Get(':id')
   findById(@Param('id') id: string) {
     return this.roleService.findById(id);
   }
 
+  @Permissions(PermissionEnum.UPDATE_ROLE)
   @Patch('/update/:id')
   update(@Param('id') id: string, @Body() dto: UpdateRoleDto) {
     return this.roleService.update(id, dto);
   }
 
+  @Permissions(PermissionEnum.DELETE_ROLE)
   @Delete('/delete/:id')
   remove(@Param('id') id: string) {
     return this.roleService.remove(id);
